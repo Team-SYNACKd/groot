300ea9904e982f4e763631d91d33d7da938ea719
Aniketh01 <anikethgireesh@gmail.com>
2020-06-13 20:08:37 +0530

    HTTP common: Introduce URL class
    This fixes #3

--- http_common.py
+++ http_common.py
@@ -1,3 +1,30 @@
+from urllib.parse import urlparse
+
+class URL:
+    def __init__(self, url: str) -> None:
+        parsed = urlparse(url)
+
+        self.scheme = parsed.scheme
+        self.authority = parsed.netloc
+        if ":" in parsed.netloc:
+            self.host, port_str = parsed.netloc.split(":")
+            self.port = int(port_str)
+        else:
+            self.host = parsed.netloc
+            if self.scheme == "http":
+                self.port = 80
+            elif self.scheme == "https":
+                self.port = 443
+            else: # NOTE: this is set to talk to our HTTP server only
+                self.port = 8000
+
+        if parsed.path:
+            self.full_path = parsed.path
+        else:
+            self.full_path = '/'
+        if parsed.query:
+            self.full_path += "?" + parsed.query
+
 class HTTPRequest:
     def __init__(self, data: bytes):
         self.method = None
