7beb789fffbc27a972ff469fc48e06131c02f264
Aniketh01 <anikethgireesh@gmail.com>
2020-06-22 14:19:48 +0530

    Logging: Introduce logging in server

--- server.py
+++ server.py
@@ -1,6 +1,9 @@
 import argparse
+import logging
 
 from server.http_server import HTTPServer
+
+logger = logging.getLogger("Server")
 
 def main():
     parser = argparse.ArgumentParser(description="HTTP/1.1 server")
@@ -16,7 +19,16 @@ def main():
         default=8080,
         help="listen on the specified port (defaults to 8080)",
     )
+    parser.add_argument(
+        "-v", "--verbose", action="store_true", help="increase logging verbosity"
+    )
+
     args = parser.parse_args()
+
+    logging.basicConfig(
+        format="%(asctime)s %(levelname)s %(name)s %(message)s",
+        level=logging.DEBUG if args.verbose else logging.INFO,
+    )
 
     server = HTTPServer(args.host, args.port)
     server.start()
