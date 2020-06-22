15ecf3587a38ce568243a813e0d0d68b52723c32
Aniketh01 <anikethgireesh@gmail.com>
2020-06-22 14:11:01 +0530

    Logging: Introduce logging in client

--- client.py
+++ client.py
@@ -1,10 +1,13 @@
 import sys
 import argparse
+import logging
 
 from client.http_client import HTTPClient
 
 from common.url import URL
 from common.http import HTTPRequest
+
+logger = logging.getLogger("Client")
 
 def main():
     parser = argparse.ArgumentParser(description="HTTP/1.1 client")
@@ -14,11 +17,19 @@ def main():
     parser.add_argument(
         "--port", type=int, help="local port to bind for connections",
     )
+    parser.add_argument(
+        "-v", "--verbose", action="store_true", help="increase logging verbosity"
+    )
 
     args = parser.parse_args()
 
+    logging.basicConfig(
+        format="%(asctime)s %(levelname)s %(name)s %(message)s",
+        level=logging.DEBUG if args.verbose else logging.INFO,
+    )
+
     for url in args.url:
-        print("sending request to {}\n".format(url))
+        logger.info("sending request to {}\n".format(url))
         request = HTTPRequest(uri=URL(url))
         client = HTTPClient(request)
 
