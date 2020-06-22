4a42a602b6fa98660348d049d7837961822ea01d
Aniketh01 <anikethgireesh@gmail.com>
2020-06-22 14:17:57 +0530

    Server: Initial CLI parsing for server

--- server.py
+++ server.py
@@ -1,5 +1,25 @@
+import argparse
+
 from server.http_server import HTTPServer
 
-if __name__ == '__main__':
-    server = HTTPServer("127.0.0.1", 8000)
+def main():
+    parser = argparse.ArgumentParser(description="HTTP/1.1 server")
+    parser.add_argument(
+        "--host",
+        type=str,
+        default="127.0.0.1",
+        help="listen on the specified address (defaults to ::)",
+    )
+    parser.add_argument(
+        "--port",
+        type=int,
+        default=8080,
+        help="listen on the specified port (defaults to 8080)",
+    )
+    args = parser.parse_args()
+
+    server = HTTPServer(args.host, args.port)
     server.start()
+
+if __name__ == '__main__':
+    main()
